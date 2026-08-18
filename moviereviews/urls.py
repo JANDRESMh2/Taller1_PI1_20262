25f7eca99159cf37aa8029192562aae06917be32
JANDRESMh2 <158773979+JANDRESMh2@users.noreply.github.com>
2026-08-18 11:30:49 -0500

    creacion de app news

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include 
 from django.urls import path
 
 from django.conf.urls.static import static 
@@ -23,8 +24,9 @@ from django.conf import settings
 from movie import views as movieViews
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', movieViews.home),
-    path('about/', movieViews.about),
+    path('', movieViews.home, name = 'home'),
+    path('about/', movieViews.about, name='about'),
+    path('news/',include('news.urls')),
 ]
 
 
